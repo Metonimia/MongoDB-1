47477917425f73b3be6b0f8e47c6cc9d531454ac
Zuzia Zuzewicz <zuzannazuzewicz@MacBook-Air-Zuzia.local>
2018-02-28 17:24:09 +0100

    Replaced a local database with mLab database

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/nodeappdatabase", {
+mongoose.connect("mongodb://z.zuzewicz:12345@ds249718.mlab.com:49718/database-1", {
 	useMongoClient: true
 });
 
